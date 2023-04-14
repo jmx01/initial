c283a68e189823ff0acd98aa095bbf2bf81f305e
jmx01 <youremail@yourdomain.com>
2023-04-14 20:10:39 +0800

    new version

--- generate_function.py
+++ generate_function.py
@@ -94,6 +94,7 @@ def change_solution(GS_new, string):
 
         GS = [meng_1, meng_2, meng_3]
         return GS
+
     elif string == "yuan":
         GY = copy.deepcopy(GS_new)
         ma_in = copy.deepcopy(GY[0])
@@ -132,63 +133,62 @@ def sequence_add(table, sequence):
     return sequence
 
 
-def sequence_decode(sequence_M, sequence_P, no_pick_zone):
-    """
-    针对一个固定的零件管序列，原料管的解生成,未知有解
-    :param no_pick_zone: 标准禁接区
-    :param sequence_M: 输入的原材料序列，原材料用长度作为标识
-    :param sequence_P: 零件序列，零件用名称作为标识，已知长度，格式为[编号，长度]
-    :return: 解格式
-    """
-    solve = copy.deepcopy(sequence_M)
-    change_zone = ["change", []]
-    cut_num = []  # 累积零件管长，最终为零件管总长
-    cal_ma = [0, 0]  # 累积原料长度,左为上次终点，右为下次使用的点
-
-    for i in range(len(sequence_P)):
-        name = sequence_P[i][0]
-        index = np.where(no_pick_zone[0] == name)
-        change_zone[1].extend(no_pick_zone[index][1])
-        cut_num.append(cut_num[-1] + no_pick_zone[index][1])
-
-    change_zone = [change_zone]
-    change_zone, change_zone_cal = standard_no_pick_zone(change_zone)  # 生成变化后禁接区和累积禁接区
-    change_zone_cal = change_zone_cal[1]
-
-    i = 0  # 第几根原料管
-    j = 0  # 在第几个禁接区
-    k = [0, 0]  # 覆盖零件管区间
-    while cal_ma[1] < cut_num[-1] and i <= len(solve):
-        cal_ma[1] = cal_ma[0] + solve[i]
-        solve.insert(i, [solve[i], 0, [], 0, 0])
-        del solve[i + 1]
-
-        while cal_ma[1] > change_zone_cal[j][0]:
-            j += 1
-
-
-        if j != 0:
-            over = cal_ma[1] - change_zone_cal[j - 1][0]  # 超出上一个区域长度
-        else:
-            over = cal_ma[1]
-
-        if change_zone_cal[j][1] == 0:  # 判断剩余长度
-            if over >= solve[i][0]:
-                solve[i][1], solve[i][3] = solve[i][0], solve[i][0]
-                solve[i].append(1)
-            else:
-                solve[i].append(2)
-        else:
-
-        cal_ma[0] = cal_ma[1]
-        k[0] = k[1]
-        i += 1
-
-    if cal_ma > cut_num[-1] and i <= len(solve)+1:
-        return solve
-    else:
-        print("序列解生成出错")
-        return 0
+# def sequence_decode(sequence_M, sequence_P, no_pick_zone):
+#     """
+#     针对一个固定的零件管序列，原料管的解生成,未知有解
+#     :param no_pick_zone: 标准禁接区
+#     :param sequence_M: 输入的原材料序列，原材料用长度作为标识
+#     :param sequence_P: 零件序列，零件用名称作为标识，已知长度，格式为[编号，长度]
+#     :return: 解格式
+#     """
+#     solve = copy.deepcopy(sequence_M)  #　深复制［长度，剩余长度，[切割向量]，有效使用长度，剩余长度］
+#     change_zone = ["change", []]  # 创建一个整体的禁接区同禁接矩阵形式吻合
+#     cut_num = [0]  # 累积零件管长，最终为零件管总长
+#     cal_ma = [0, 0]  # 累积原料长度,左为上次终点，右为下次使用的点
+#
+#     for i in range(len(sequence_P)):  # 对所有零件管
+#         name = sequence_P[i][0] # 给出所有零件管的name str
+#         index = np.where(no_pick_zone[0] == name) # 给出对应零件的禁接区
+#         change_zone[1].extend(no_pick_zone[index][1]) # 将禁接区加到change_zone上
+#         cut_num.append(cut_num[-1] + no_pick_zone[index][1])
+#
+#     change_zone, change_zone_cal = standard_no_pick_zone([change_zone])  # 生成变化后禁接区和累积禁接区
+#     change_zone_cal = change_zone_cal[0][1]
+#
+#     i = 0  # 第几根原料管
+#     j = 0  # 在第几个禁接区
+#     k = [0, 0]  # 覆盖零件管区间
+#     while cal_ma[1] < cut_num[-1] and i <= len(solve)-1:  # 当前原料总长小于零件管总长，且仍有原料管时
+#         cal_ma[1] = cal_ma[0] + solve[i]
+#         solve.insert(i, [solve[i], 0, [], 0, 0])
+#         del solve[i + 1]
+#
+#         while cal_ma[1] > change_zone_cal[j][0]:
+#             j += 1
+#
+#
+#         if j != 0:
+#             over = cal_ma[1] - change_zone_cal[j - 1][0]  # 超出上一个区域长度
+#         else:
+#             over = cal_ma[1]
+#
+#         if change_zone_cal[j][1] == 0:  # 判断剩余长度
+#             if over >= solve[i][0]:
+#                 solve[i][1], solve[i][3] = solve[i][0], solve[i][0]
+#                 solve[i].append(1)
+#             else:
+#                 solve[i].append(2)
+#         else:
+#
+#         cal_ma[0] = cal_ma[1]
+#         k[0] = k[1]
+#         i += 1
+#
+#     if cal_ma[] > cut_num[-1] and i <= len(solve):
+#         return solve
+#     else:
+#         print("序列解生成出错")
+#         return 0
 
 
 data = initial_data()
